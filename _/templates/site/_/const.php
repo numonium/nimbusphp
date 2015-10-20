4ba34c118bfa372a9ac74b16ff0291b7f8a4b686
Eric Newman <eric@ultra.me>
2015-10-20 13:32:44 -0400

    remove extraneous data

--- _/templates/site/_/const.php
+++ _/templates/site/_/const.php
@@ -2,44 +2,13 @@
 	global $_;
 	
 	$_['const']['sections'] = array(
-		'vacations'	=> 'Vacations',
-		'automotive-solutions'	=> 'Automotive Solutions',
-		'billboards-tv-ads'		=> 'Billboards &amp; TV Ads',
-#		'tire-revenue'			=> 'Tire Revenue',
-		'ntr-events'			=> 'NTR Events',
-		'bill-pay'				=> 'Bill Pay',
-		'vehicle-wraps'			=> 'Vehicle Wraps',
-		'station-merchandise'	=> 'Station Merchandise',
-		'one-stop-trade-shop'	=> 'One-Stop Trade Shop',
-		'gift-cards'			=> 'Gift Cards'
+		# slug => title
 	);
 	
 	$_['const']['admin']['sections'] = array(
-		'admin-orders'		=> 'Orders',
-		'admin-reservations'	=> 'Reservations',
-		'admin-stations'		=> 'Stations',
-		'admin-vacations'		=> 'Vacations',
-		'admin-analytics'		=> 'Analytics',
-		'admin-leads'			=> 'Leads',
-		'admin-promotions'	=> 'Promotions'	,
-		'admin-certificates'	=> 'Certificates',
-		'admin-confirmation-letters'	=> 'Confirmation Letters',
-		'admin-documents'		=> 'Documents',
-		'admin-emails'		=> 'Emails',
-		'admin-users'			=> 'Users',
-		'admin-settings'		=> 'Settings'
 	);
 	
 	$_['const']['link'] = array(
-		'old'	=> array(
-			'admin'	=> array(
-				'login'	=> 'http://old.sunfunmedia.com/MyAccount/?login',
-				'order'	=> 'http://old.sunfunmedia.com/MyAccount/TravelOrder/orderpkg.php',
-				'register'	=> 'http://old.sunfunmedia.com/MyAccount/RegisterPkg/unregisteredpkgs.php',
-				'validuser'	=> 'http://old.sunfunmedia.com/MyAccount/validuser.php',
-				'forgot-pw'	=> 'http://old.sunfunmedia.com/MyAccount/PwEval/eval_forgot_pw.php'
-			)
-		)
 	);
 	
 ?>
